3a85ad8b702335fbd22033541be55ad7b889ad5e
Chris <chris@string.co>
2020-05-18 11:27:57 +0200

    String versions of the Coconut templates

---
+++ templates/StringBaseCoconut.xctemplate/ModelControllerNetworking/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by: ___FULLUSERNAME___ on ___DATE___
+// 
+//  Copyright © ___YEAR___ String Applied Technologies Limited. 
+//  This file and its contents are proprietary to String Applied Technologies Limited 
+//  and are strictly confidential. Any unauthorised distribution or use may result in 
+//  legal action, including criminal sanctions.
+//
+
+import Foundation
+protocol ___VARIABLE_sceneName___Presenter {
+    
+    init()
+    
+    var baseViewController: ___VARIABLE_sceneName___ViewController! { get set }
+}
